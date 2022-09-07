89aa8e555e5e5cc2a3202abddad2bc5be241719e
andresyrg16 <andresyrg16@gmail.com>
2022-09-07 08:10:37 -0500

    toCamelCase resuelto

---
+++ codeWar/toCamelCase.js
@@ -0,0 +1,18 @@
+function toCamelCase(str) {
+  let results = ""
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "-" || str[i-1] === "_") {
+      continue;
+    }
+    if (str[i - 1] === "-" || str[i-1] === "_") {
+      results = results + str[i].toUpperCase();;
+      continue;
+    }
+    results = results + str[i]
+  }
+  return results;
+}
+
+console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("A-B-C"));
